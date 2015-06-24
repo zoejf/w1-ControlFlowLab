b3bc23d3b27ce5828ed8373fd9a45e4f173ef3a1
zoejf <zoejfoss@gmail.com>
2015-06-24 16:56:05 -0700

    Exc. 1 - finished prompt to set number of verses

--- snippets.js
+++ snippets.js
@@ -1,14 +1,22 @@
-for (i=5;i>0;i--) {
-    if (i>1){
+var verses = prompt("How many verses do you want to sing?");
+
+for (i=verses; i>0; i--) {
+    if (i>2){
        console.log(i + " bottles of beer on the wall,");
        console.log(i + " bottles of beer!");
        console.log("Take one down and pass it around,"); 
        console.log((i-1) + " bottles of beer on the wall!");
     }
+    else if (i===2) {
+       console.log(i + " bottles of beer on the wall,");
+       console.log(i + " bottles of beer!");
+       console.log("Take one down and pass it around,"); 
+       console.log((i-1) + " bottle of beer on the wall!");
+    }
     else if (i===1) {
        console.log(i + " bottle of beer on the wall,");
        console.log(i + " bottle of beer!");
        console.log("Take one down and pass it around,");
-       console.log("'No more bottles of beer on the wall!");
+       console.log("No more bottles of beer on the wall!");
     }   
 }
